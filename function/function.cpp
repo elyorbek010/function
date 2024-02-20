b26def777b61d0ceace230aef82022848baeb112
Elyorbek Kholikov <elyorbek4111020@list.ru>
2024-02-20 19:35:50 +0500

    Add project files.

---
+++ function/function.cpp
@@ -0,0 +1,12 @@
+﻿// function.cpp : Defines the entry point for the application.
+//
+
+#include "function.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
